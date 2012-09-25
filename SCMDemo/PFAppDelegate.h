6f937e67ab913297190b9791fbbd338487645424
Andy-24 <pengfei24@gmail.com>
2012-09-25 11:14:24 +0800

    Initial Commit

---
+++ SCMDemo/PFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PFAppDelegate.h
+//  SCMDemo
+//
+//  Created by 彭飞 on 12-9-25.
+//  Copyright (c) 2012年 彭飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SCMDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SCMDemo
+//
+//  Created by 彭飞 on 12-9-25.
+//  Copyright (c) 2012年 彭飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PFAppDelegate class]));
+    }
+}
