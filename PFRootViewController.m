d220575a833dddf36993f213a1e7955cd16f540b
Andy-24 <pengfei24@gmail.com>
2012-09-25 12:27:01 +0800

    Initial commit. LOL.

---
+++ PFRootViewController.m
@@ -0,0 +1,38 @@
+//
+//  PFRootViewController.m
+//  SCMDemo
+//
+//  Created by 彭飞 on 12-9-25.
+//  Copyright (c) 2012年 彭飞. All rights reserved.
+//
+
+#import "PFRootViewController.h"
+
+@interface PFRootViewController ()
+
+@end
+
+@implementation PFRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
